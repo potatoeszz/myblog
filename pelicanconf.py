e5c752f75d1933d6a641a6cef6dc2150a3d0b8a0
potatoeszz <boopioz101@gmail.com>
2024-11-24 15:04:20 -0500

    changed the theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,9 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
 
+THEME = "/Users/gloriasun/pelican-themes/iris"
+
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
